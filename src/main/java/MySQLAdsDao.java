069d6b90271b33f8f1524938b5cfbfa331d77d59
Brian Barton <brian.n.barton@gmail.com>
2020-09-22 10:09:32 -0600

    sessions

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private connection Connection;
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+}
