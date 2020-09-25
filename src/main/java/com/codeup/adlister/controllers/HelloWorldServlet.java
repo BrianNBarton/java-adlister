587b654b710628b2930439e8ba313ba001174c5e
Brian Barton <brian.n.barton@gmail.com>
2020-09-25 17:27:46 -0600

    oh boy

--- src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
+++ src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
@@ -5,10 +5,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String name = req.getParameter("name");
+        out.println(String.format("<h1>Hello %s!</h1>", name != null ? name : "World"));
     }
 }
